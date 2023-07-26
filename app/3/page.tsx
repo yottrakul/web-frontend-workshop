75c1a272304d983a93b00b58eb54091f9e24538c
Yottrakul Suwannasri <yottrakul_s.vo@tripetch-it.co.th>
2023-07-26 11:49:31 +0700

    Testimonial-Food-Delivery-Landing-Page

---
+++ app/3/page.tsx
@@ -0,0 +1,61 @@
+export default function FoodDelivery() {
+  return (
+    <section className="py-[14.38rem] font-poppin">
+      <div className="container mx-auto min-w-fit">
+        <div>
+          <span className="mb-[0.81rem] block text-center font-semibold uppercase tracking-[0.42rem] text-[#FF1919]">
+            success stories
+          </span>
+          <h1 className="mb-[4.5rem] text-center text-[3.1875rem]">
+            <span className="inline-block whitespace-nowrap">Get to know</span>{' '}
+            <span className="inline-block">about us</span>
+          </h1>
+        </div>
+        <div className="flex flex-wrap justify-center gap-[2.5rem]">
+          <div className="border-wrapper flex-none rounded-2xl bg-gradient-to-br from-white via-white to-[#FFC4BC] p-[2px]">
+            <div className=" rounded-2xl bg-[linear-gradient(135deg,_#FFFAFA_0%,_#FFF7F6_100%)] px-[2.75rem] py-[2.63rem]">
+              <div className="grid h-[11.6rem] w-[12.2rem] place-items-center rounded-[0.88rem] bg-white  text-center font-semibold shadow-[0px_40px_80px_0px_#FBE4E0]">
+                <div className="wrapper">
+                  <span className="mb-4 block text-[2.75rem] leading-[2.8125rem] tracking-[-0.0275rem] text-[#292019]">
+                    22k+
+                  </span>
+                  <span className="block text-[0.8125rem] leading-[1.125rem] text-[#62564D]">
+                    Use Order Online
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="border-wrapper flex-none rounded-2xl bg-gradient-to-br from-white via-white to-[#FFC4BC] p-[2px]">
+            <div className=" rounded-2xl bg-[linear-gradient(135deg,_#FFFAFA_0%,_#FFF7F6_100%)] px-[2.75rem] py-[2.63rem]">
+              <div className="grid h-[11.6rem] w-[12.2rem] place-items-center rounded-[0.88rem] bg-white  text-center font-semibold shadow-[0px_40px_80px_0px_#FBE4E0]">
+                <div className="wrapper">
+                  <span className="mb-4 block text-[2.75rem] leading-[2.8125rem] tracking-[-0.0275rem] text-[#292019]">
+                    459
+                  </span>
+                  <span className="block text-[0.8125rem] leading-[1.125rem] text-[#62564D]">
+                    Trusted Clients
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="border-wrapper flex-none rounded-2xl bg-gradient-to-br from-white via-white to-[#FFC4BC] p-[2px]">
+            <div className=" rounded-2xl bg-[linear-gradient(135deg,_#FFFAFA_0%,_#FFF7F6_100%)] px-[2.75rem] py-[2.63rem]">
+              <div className="grid h-[11.6rem] w-[12.2rem] place-items-center rounded-[0.88rem] bg-white  text-center font-semibold shadow-[0px_40px_80px_0px_#FBE4E0]">
+                <div className="wrapper">
+                  <span className="mb-4 block text-[2.75rem] leading-[2.8125rem] tracking-[-0.0275rem] text-[#292019]">
+                    12k+
+                  </span>
+                  <span className="block text-[0.8125rem] leading-[1.125rem] text-[#62564D]">
+                    Use Order Online
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
